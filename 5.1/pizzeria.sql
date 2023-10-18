9a49ec16db377e8937dc22b396512a41106feba2
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2023-10-18 17:43:26 +0200

    fixing the pizzeria script

--- 5.1/pizzeria.sql
+++ 5.1/pizzeria.sql
@@ -1,30 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS `Pizzeria` DEFAULT CHARACTER SET utf8 ;
-
-CREATE TABLE IF NOT EXISTS `Pizzeria`.`stores` (
-  `id_stores` INT NOT NULL,
-  `adress` VARCHAR(120) NOT NULL,
-  `postal_code` VARCHAR(5) NOT NULL,
-  `locality` VARCHAR(45) NOT NULL,
-  `province` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_stores`))
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `Pizzeria`.`employees` (
-  `id_employee` INT NOT NULL,
-  `store_id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `surname` VARCHAR(45) NOT NULL,
-  `NIF` VARCHAR(9) NOT NULL,
-  `telephone` VARCHAR(20) NOT NULL,
-  `occupation` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_employee`),
-  INDEX `store_id_idx` (`store_id` ASC) VISIBLE,
-  CONSTRAINT `store_id`
-    FOREIGN KEY (`store_id`)
-    REFERENCES `Pizzeria`.`stores` (`id_stores`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE SCHEMA IF NOT EXISTS `Pizzeria`;
 
 CREATE TABLE IF NOT EXISTS `Pizzeria`.`clients` (
   `id_client` INT NOT NULL,
@@ -38,6 +12,15 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`clients` (
   PRIMARY KEY (`id_client`))
 ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `Pizzeria`.`stores` (
+  `id_stores` INT NOT NULL,
+  `adress` VARCHAR(120) NOT NULL,
+  `postal_code` VARCHAR(5) NOT NULL,
+  `locality` VARCHAR(45) NOT NULL,
+  `province` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_stores`))
+ENGINE = InnoDB;
+
 CREATE TABLE IF NOT EXISTS `Pizzeria`.`orders` (
   `id_orders` INT NOT NULL,
   `store_id` INT NOT NULL,
@@ -49,14 +32,31 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`orders` (
   PRIMARY KEY (`id_orders`, `client_id`, `store_id`),
   INDEX `store_id_idx` (`store_id` ASC) VISIBLE,
   INDEX `client_id_idx` (`client_id` ASC) VISIBLE,
-  CONSTRAINT `store_id`
+  CONSTRAINT `orders_store_id`
     FOREIGN KEY (`store_id`)
     REFERENCES `Pizzeria`.`stores` (`id_stores`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `client_id`
+  CONSTRAINT `orders_client_id`
     FOREIGN KEY (`client_id`)
     REFERENCES `Pizzeria`.`clients` (`id_client`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Pizzeria`.`employees` (
+  `id_employee` INT NOT NULL,
+  `store_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `surname` VARCHAR(45) NOT NULL,
+  `NIF` VARCHAR(9) NOT NULL,
+  `telephone` VARCHAR(20) NOT NULL,
+  `occupation` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_employee`),
+  INDEX `store_id_idx` (`store_id` ASC) VISIBLE,
+  CONSTRAINT `employee_store_id`
+    FOREIGN KEY (`store_id`)
+    REFERENCES `Pizzeria`.`stores` (`id_stores`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -69,12 +69,12 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`delivery` (
   PRIMARY KEY (`id_delivery`, `order_id`, `employee_id`),
   INDEX `employee_id_idx` (`employee_id` ASC) VISIBLE,
   INDEX `order_id_idx` (`order_id` ASC) VISIBLE,
-  CONSTRAINT `employee_id`
+  CONSTRAINT `delivery_employee_id`
     FOREIGN KEY (`employee_id`)
     REFERENCES `Pizzeria`.`employees` (`id_employee`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `order_id`
+  CONSTRAINT `delivery_order_id`
     FOREIGN KEY (`order_id`)
     REFERENCES `Pizzeria`.`orders` (`id_orders`)
     ON DELETE NO ACTION
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`pizzas` (
   `price` DECIMAL(5,2) NOT NULL,
   PRIMARY KEY (`id_pizzas`, `category_id`),
   INDEX `category_id_idx` (`category_id` ASC) VISIBLE,
-  CONSTRAINT `category_id`
+  CONSTRAINT `p_category_id`
     FOREIGN KEY (`category_id`)
     REFERENCES `Pizzeria`.`pizza_categories` (`id_pizza_categories`)
     ON DELETE NO ACTION
@@ -132,22 +132,22 @@ CREATE TABLE IF NOT EXISTS `Pizzeria`.`items_counter` (
   INDEX `burguer_id_idx` (`burguer_id` ASC) VISIBLE,
   INDEX `pizza_id_idx` (`pizza_id` ASC) VISIBLE,
   INDEX `drink_id_idx` (`drink_id` ASC) VISIBLE,
-  CONSTRAINT `order_id`
+  CONSTRAINT `items_counter_order_id`
     FOREIGN KEY (`order_id`)
     REFERENCES `Pizzeria`.`orders` (`id_orders`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `burguer_id`
+  CONSTRAINT `items_counter_burguer_id`
     FOREIGN KEY (`burguer_id`)
     REFERENCES `Pizzeria`.`burguers` (`id_burguers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `pizza_id`
+  CONSTRAINT `items_counter_pizza_id`
     FOREIGN KEY (`pizza_id`)
     REFERENCES `Pizzeria`.`pizzas` (`id_pizzas`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `drink_id`
+  CONSTRAINT `items_counter_drink_id`
     FOREIGN KEY (`drink_id`)
     REFERENCES `Pizzeria`.`drinks` (`id_drinks`)
     ON DELETE NO ACTION
